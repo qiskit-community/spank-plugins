b6fc65132d6f2889e93a7f86b12f761c7d193234
OkuyanBoga <mustafaemresahin@outlook.com>
2025-04-04 17:08:57 +0100

    bugfix for QRS primitives

--- primitives/python/qrmi_primitives/ibm/__init__.py
+++ primitives/python/qrmi_primitives/ibm/__init__.py
@@ -14,10 +14,14 @@
 
 from .direct_access_estimator import (
     IBMDirectAccessEstimatorV2,
-    IBMQiskitRuntimeServiceEstimatorV2,
 )
 from .direct_access_sampler import (
     IBMDirectAccessSamplerV2,
+)
+from .qiskit_runtime_service_estimator import (
+    IBMQiskitRuntimeServiceEstimatorV2,
+)
+from .qiskit_runtime_service_sampler import (
     IBMQiskitRuntimeServiceSamplerV2,
 )
 from .runtime_job_v2 import RuntimeJobV2

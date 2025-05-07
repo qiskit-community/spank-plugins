875cf630c1554c83cbc83530536ee1d6134559b4
Munetaka Ohtani <ohtanim@jp.ibm.com>
2025-05-07 21:02:10 +0900

    Update INSTALL.md (#70)
    * Update INSTALL.md
    
    * minor fixes
    
    * add demo/qrmi

---
+++ demo/qrmi/jobs/run_sampler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=sampler_job
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --qpu=test_heron,test_eagle
+# #SBATCH --qpu=alt_marrakesh
+# #SBATCH --qpu=alt_marrakesh,test_heron
+
+# Your script goes here
+source /shared/pyenv/bin/activate
+srun python /shared/spank-plugins/primitives/python/examples/ibm/sampler.py

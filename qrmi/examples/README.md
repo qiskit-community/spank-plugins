063fc261381c1f84329cbc8437b2f3671dee0133
Munetaka Ohtani <ohtanim@jp.ibm.com>
2025-06-06 12:14:27 +0900

    Add STFC copyright and Apache 2 license text for QRMI

--- qrmi/examples/README.md
+++ qrmi/examples/README.md
@@ -3,3 +3,7 @@
 * [Examples in Rust](./rust)
 * [Examples in Python](./python)
 * [Examples in C](./c)
+
+## License
+
+[Apache-2.0](https://github.com/qiskit-community/spank-plugins/blob/main/qrmi/LICENSE.txt)
--- qrmi/src/ibm.rs
+++ qrmi/src/ibm.rs
@@ -10,6 +10,18 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
+// This code is part of Qiskit.
+//
+// Copyright (C): 2025 UKRI-STFC (Hartree Centre)
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
 //! QRMI implementations for IBM Quantum services
 
 mod direct_access;

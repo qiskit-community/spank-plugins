e14665b05467b3783677339b285ef553594d9b81
Munetaka Ohtani <ohtanim@jp.ibm.com>
2025-08-07 20:46:03 +0900

    update demo files

--- demo/qrmi/jobs/run_sampler.sh
+++ demo/qrmi/jobs/run_sampler.sh
@@ -3,10 +3,8 @@
 #SBATCH --job-name=sampler_job
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --qpu=test_heron,test_eagle
-# #SBATCH --qpu=alt_marrakesh
-# #SBATCH --qpu=alt_marrakesh,test_heron
+#SBATCH --qpu=ibm_sherbrooke,ibm_brisbane
 
 # Your script goes here
 source /shared/pyenv/bin/activate
-srun python /shared/spank-plugins/primitives/python/qiskit_qrmi_primitives/examples/ibm/sampler.py
+srun python /shared/qrmi/examples/qiskit_primitives/ibm/sampler.py
--- demo/qrmi/jobs/run_task.sh
+++ demo/qrmi/jobs/run_task.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=estimator_job
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --qpu=ibm_sherbrooke,ibm_brisbane
+#SBATCH --qpu=ibm_sherbrooke
 
 # Your script goes here
 source /shared/pyenv/bin/activate
-srun python /shared/qrmi/examples/qiskit_primitives/ibm/estimator.py
+srun task_runner ibm_sherbrooke /shared/qrmi/bin/task_runner/examples/qiskit/estimator_input_ibm_sherbrooke.json
